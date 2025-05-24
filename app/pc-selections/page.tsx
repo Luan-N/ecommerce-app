27487661f043809be05bc3790516e838f3636b0a
Luan Nguyen <luannguyen10282004@gmail.com>
2025-05-24 00:46:22 -0700

    navigation skeleton done

---
+++ app/pc-selections/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Products() {
+    return (
+        <div>
+            <h1>Products</h1>
+            <p>Here you can find a list of our products.</p>
+        </div>
+    );
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,44 @@
+"use client"
+
 import Link from "next/link";
+import { RiMenuFold2Fill } from "react-icons/ri";
+import { VscClose } from "react-icons/vsc";
+import { useState } from "react";
 
 export default function NavBar() {
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
-    <nav className="bg-gray-800 p-4">
-      <div>
-        <Link href="/">My E-Commerce</Link>
+    <nav className="flex justify-between px-5 items-center border-b h-15 fixed w-full z-50">
+
+      {/* Wide Screen Navigation */}
+      <div className="hidden md:flex">
+        <Link href="/" className="hover:border-b-2">My E-Commerce</Link>
       </div>
+      <div className="hidden md:flex gap-4">
+        <Link href="/" className="navbtn">Home</Link>
+        <Link href="/pc-selections" className="navbtn">PC Selections</Link>
+        <Link href="/components" className="navbtn">Components</Link>
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="md:hidden">
+        <button className="text-2xl cursor-pointer" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+          {isMobileMenuOpen ? <VscClose /> : <RiMenuFold2Fill/>}
+        </button>
+        {isMobileMenuOpen && (
+          <div className="absolute top-16 left-0 bg-[#FAFAFA] w-full shadow-lg p-4">
+            <Link href="/" className="block py-2 hover:border-b-2">Home</Link>
+            <Link href="/pc-selections" className="block py-2 hover:border-b-2">PC Selections</Link>
+            <Link href="/components" className="block py-2 hover:border-b-2">Components</Link>
+          </div>
+        )}
+      </div>
+      <div>
+        <Link href="/cart" className="hover:border-b-2">Cart</Link>
+      </div>
+
     </nav>
   );
 }
