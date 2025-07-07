9a50e0fb6cefba2feab33ba330b631be0af5659b
Luan Nguyen <luannguyen10282004@gmail.com>
2025-07-07 01:24:57 -0700

    tos and privacy pages done

---
+++ app/tos/page.tsx
@@ -0,0 +1,21 @@
+export default function TermsOfServicePage() {
+    return (
+            <main className="my-25 mx-5 md:mx-15">
+            <h1 className="text-2xl font-bold my-5 border-b">Terms and Services</h1>
+            <p>Welcome to PC Linker! By using our website, you agree to comply with and be bound by the following terms and conditions of use. Please read them carefully.</p>
+            <h2 className="text-xl font-bold my-2">Acceptance of Terms</h2>
+            <p>By accessing or using our website, you confirm that you accept these terms and conditions and that you agree to comply with them. If you do not agree to these terms, you must not use our website.</p>
+            <h2 className="text-xl font-bold my-2">Changes to Terms</h2>
+            <p>We may revise these terms of service from time to time. The revised terms will be effective immediately upon posting on this page. Your continued use of the website after any changes indicates your acceptance of the new terms.</p>
+            <h2 className="text-xl font-bold my-2">Use of the Website</h2>
+            <p>You may use our website for lawful purposes only. You must not use our site in any way that violates any applicable local, national, or international law or regulation.</p>
+            <h2 className="text-xl font-bold my-2">Limitation of Liability</h2>
+            <p>PC Linker and its data are provided “as is,” with no guarantees of accuracy or uptime.</p>
+            <p>To the fullest extent permitted by law, PC Linker will not be liable for any direct, indirect, incidental, special, consequential, or punitive damages arising out of or related to your use of the website, even if we have been advised of the possibility of such damages. This includes, but is not limited to, damages for loss of profits, goodwill, use, data, or other intangible losses.</p>
+            <h2 className="text-xl font-bold my-2">Governing Law</h2>
+            <p>These terms of service are governed by and construed in accordance with the laws of your jurisdiction, without regard to its conflict of law principles. Any disputes arising out of or related to these terms or your use of the website will be subject to the exclusive jurisdiction of the courts in your jurisdiction.</p>
+
+            <p>Last Updated: July 7, 2025</p>
+        </main>
+    )
+}
